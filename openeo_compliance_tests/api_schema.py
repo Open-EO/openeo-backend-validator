71d331495712e6bc311789521ef4e0f3823fa1a2
Jeroen Dries <jeroen.dries@vito.be>
2019-01-14 15:11:58 +0100

    added test for collections

--- openeo_compliance_tests/api_schema.py
+++ openeo_compliance_tests/api_schema.py
@@ -1,6 +1,20 @@
-import requests
 
-schema = requests.get("https://raw.githubusercontent.com/Open-EO/openeo-api/master/openapi.json").json()
+import json
+import pkgutil
+
+
+schema = json.loads(pkgutil.get_data("openeo_compliance_tests", "openapi.json"))
 
 capabilities_schema = schema["paths"]["/"]["get"]["responses"]["200"]["content"]["application/json"]["schema"]
 processes_schema = schema["paths"]["/processes"]["get"]["responses"]["200"]["content"]["application/json"]["schema"]
+
+collections_schema = schema["paths"]["/collections"]["get"]["responses"]["200"]["content"]["application/json"]["schema"]
+
+#import sys
+#sys.setrecursionlimit(20000)
+#import json
+#with open("/home/driesj/pythonworkspace/openeo-python-compliance/openapi031-nonrecursive.json","r") as f:
+#    schema_non_recursive = json.load(f)
+
+#from openapi_core import create_spec
+#openapi_spec = create_spec(schema_non_recursive)
---
+++ openeo_compliance_tests/test_collections.py
@@ -0,0 +1,15 @@
+from .api_schema import collections_schema,schema
+import jsonschema
+
+def test_collections(backend):
+    """
+    Validates the collections metadata document against the json schema.
+
+    :param backend:
+    :return:
+    """
+
+    collections = backend.get_json("/collections")
+    resolver = jsonschema.RefResolver.from_schema(schema)
+    validator = jsonschema.validators.validator_for(schema)(collections_schema,resolver=resolver)
+    validator.validate(collections,collections_schema)
