4084d3d837426a75cbea2837fd284c5cbd194dc6
bgoesswe <bernhard.goesswein@geo.tuwien.ac.at>
2019-06-21 12:27:44 +0200

    Go: fixed some wrong links

--- openeoct/kin-openapi/openapi3/discriminator.go
+++ openeoct/kin-openapi/openapi3/discriminator.go
@@ -3,7 +3,7 @@ package openapi3
 import (
 	"context"
 
-	"github.com/Open-EO/openeo-backend-validator/openeoct/jsoninfo"
+	"github.com/Open-EO/openeo-backend-validator/openeoct/kin-openapi/jsoninfo"
 )
 
 // Discriminator is specified by OpenAPI/Swagger standard version 3.0.
