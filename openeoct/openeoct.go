1373c887d2563a3d469207700e362f668eb34b8a
bgoesswe <bernhard.goesswein@geo.tuwien.ac.at>
2019-07-16 12:00:50 +0200

    Go: Added external function to validate responses

--- openeoct/openeoct.go
+++ openeoct/openeoct.go
@@ -264,6 +264,180 @@ func (ct *ComplianceTest) validate(endpoint Endpoint) (string, *ErrorMessage) {
 	return "Valid", nil
 }
 
+// log.Println(ExternValidateResponse("https://raw.githubusercontent.com/Open-EO/openeo-api/0.4.1/openapi.json", "/", "GET", 200, "", ""))
+func ExternValidateResponse(openapi_path string, endpoint_path string, endpoint_type string, response_status int, response_body string, response_header string) string {
+	ct := new(ComplianceTest)
+	//ct.backend.url = config.Url
+	ct.apifile = openapi_path
+
+	endpoint := new(Endpoint)
+	endpoint.Id = "Test"
+	endpoint.Url = endpoint_path
+	endpoint.Request_type = endpoint_type
+
+	resp := new(http.Response)
+
+	resp.StatusCode = response_status
+	resp.Body = ioutil.NopCloser(strings.NewReader(response_body))
+	//resp.Header = response_header
+
+	state, err := ct.validateResponse(*endpoint, resp)
+
+	if err != nil {
+		return err.msg
+	} else {
+		return state
+	}
+
+}
+
+// Validates a single endpoint against a given response from the backend.
+// Returns the resulting state and an error message if something went wrong.
+func (ct *ComplianceTest) validateResponse(endpoint Endpoint, response *http.Response) (string, *ErrorMessage) {
+	//log.Println(openapi3.SchemaStringFormats)
+	//openapi3.DefineStringFormat("url", `^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$`)
+	//log.Println(openapi3.SchemaStringFormats)
+
+	_, err := os.Stat(ct.apifile)
+
+	// Try to read the openapi3 file
+	swagger, err := openapi3.NewSwaggerLoader().LoadSwaggerFromFile(ct.apifile)
+
+	if err != nil {
+		// openapi3 file not found, assume it is an URI
+		apiReq, _ := http.NewRequest(http.MethodGet, ct.apifile, nil)
+		swagger, err = openapi3.NewSwaggerLoader().LoadSwaggerFromURI(apiReq.URL)
+	}
+
+	if err != nil {
+		errormsg := new(ErrorMessage)
+		errormsg.msg = "Error reading the openEO API, neighter file nor url found : \n" + err.Error()
+		return "Error1", errormsg
+	}
+
+	router := openapi3filter.NewRouter().WithSwagger(swagger)
+	ctx := context.TODO()
+
+	token := ""
+
+	// Set Authentication Token
+	if ct.username != "" && ct.password != "" && ct.authendpoint != "" {
+
+		client := &http.Client{}
+
+		httpReq, _ := http.NewRequest(http.MethodGet, ct.backend.url+ct.authendpoint, nil)
+		httpReq.SetBasicAuth(ct.username, ct.password)
+		resp, _ := client.Do(httpReq)
+		if resp.StatusCode == 200 {
+			body, _ := ioutil.ReadAll(resp.Body)
+			m := make(map[string]interface{})
+			json.Unmarshal(body, &m)
+			token, _ = m["access_token"].(string)
+
+		}
+	}
+
+	// Define Request
+	httpReq, errReq := ct.buildRequest(endpoint, token, false)
+
+	if errReq != "" {
+		errormsg := new(ErrorMessage)
+		errormsg.msg = "Error processing the Config file: \n" + errReq
+		return "Error1", errormsg
+	}
+
+	// Find route in openAPI definition
+	route, pathParams, err := router.FindRoute(httpReq.Method, httpReq.URL)
+
+	if err != nil {
+		errormsg := new(ErrorMessage)
+		errormsg.msg = "Error finding endpoint in the OpenAPI definition: \n" + err.Error()
+		return "Error2", errormsg
+	}
+
+	// Options for the validation
+	options := &openapi3filter.Options{
+		AuthenticationFunc: func(c context.Context, input *openapi3filter.AuthenticationInput) error {
+			// TODO: support more schemes
+			sec := input.SecurityScheme
+			if sec.Type == "http" && sec.Scheme == "bearer" {
+				if httpReq.Header.Get("Authorization") == "" {
+					return nil //errors.New("Missing auth")
+				}
+			}
+			return nil
+		},
+	}
+
+	// Validate request
+	requestValidationInput := &openapi3filter.RequestValidationInput{
+		Request:    httpReq,
+		PathParams: pathParams,
+		Route:      route,
+		Options:    options}
+
+	if err := openapi3filter.ValidateRequest(ctx, requestValidationInput); err != nil {
+		errormsg := new(ErrorMessage)
+		errormsg.msg = "Error validating the request: \n" + err.Error()
+		return "Error3", errormsg
+	}
+
+	// Send request
+	//client := &http.Client{}
+
+	//execReq, errReq := ct.buildRequest(endpoint, token, true)
+
+	// resp, err := client.Do(execReq)
+
+	// if err != nil {
+	// 	errormsg := new(ErrorMessage)
+	// 	errormsg.msg = "Error sending request to back end: \n" + err.Error()
+	// 	return "Error4", errormsg
+	// }
+
+	// Get Response
+	body, err := ioutil.ReadAll(response.Body)
+
+	// log.Println(string(body))
+
+	if response.StatusCode == 401 {
+		errormsg := new(ErrorMessage)
+		errormsg.msg = "Error: Authentication failed, currently only BasicAuth is supported."
+		return "Error", errormsg
+	}
+
+	if err != nil {
+		errormsg := new(ErrorMessage)
+		errormsg.msg = "Error reading response from the back end: \n" + err.Error()
+		return "Error5", errormsg
+	}
+
+	var (
+		respStatus = response.StatusCode
+		respHeader = response.Header
+		respBody   = string(body)
+	)
+
+	// Define validation input
+	responseValidationInput := &openapi3filter.ResponseValidationInput{
+		RequestValidationInput: requestValidationInput,
+		Status:                 respStatus,
+		Header:                 respHeader}
+
+	if respBody != "" {
+		responseValidationInput.SetBodyBytes([]byte(respBody))
+	}
+
+	// Validate response.
+	if err := openapi3filter.ValidateResponse(ctx, responseValidationInput); err != nil {
+		errormsg := new(ErrorMessage)
+		errormsg.msg = "Response of the back end not valid: \n" + err.Error()
+		return "Not Valid", errormsg
+	}
+
+	return "Valid", nil
+}
+
 // Elements of the Config file
 
 type Config struct {
@@ -364,6 +538,8 @@ func ReadConfig(config_file string) Config {
 // 	//	ct.endpoints = []string{"/", "/collections", "/service_types"}
 
 // 	// Run validation
+// 	ct.listEndpoints()
+
 // 	result := ct.validateAll()
 
 // 	jsonString, _ := json.Marshal(result)
